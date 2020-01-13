1a553ddcfef633b9d52b4c9d1131784c8b0b6953
771351 <58735806+771351@users.noreply.github.com>
2020-01-13 07:42:17 -0600

    Adding black project

---
+++ Mastermind_JAE2/Mastermind_JAE2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Mastermind_JAE2
+//
+//  Created by BURRIGHT, JACK on 1/13/20.
+//  Copyright © 2020 BURRIGHT, JACK. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
