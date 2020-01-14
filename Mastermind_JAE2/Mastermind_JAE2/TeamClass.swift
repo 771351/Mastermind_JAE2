32cfcbb3eac42d7f5b42b8f437ded2f78ad84101
Emma Vedhuis <771511@apps.district196.org>
2020-01-14 08:04:18 -0600

    Added format to home page, coded functions to updated variables in teamClass

--- Mastermind_JAE2/Mastermind_JAE2/TeamClass.swift
+++ Mastermind_JAE2/Mastermind_JAE2/TeamClass.swift
@@ -21,6 +21,8 @@ class teamClass {
     var won7 : Int
     var won8 : Int
     var teamName : String
+    var gameGuess : Int
+    var everyGuess : Int
     
     init(name: String){
         self.teamName = name
@@ -36,5 +38,40 @@ class teamClass {
         self.won6 = 0
         self.won7 = 0
         self.won8 = 0
+        self.gameGuess = 0
+        self.everyGuess = 0
+    }
+    
+    func gameDONE(gamesP: Int, aver: Int, gameGuess: Int){
+        
+        self.gamesP = gamesP+1
+        
+        if (self.gameGuess == 1){
+            self.won1 = won1+1
+        }
+        else if (self.gameGuess == 2){
+            self.won2 = won2+1
+        }
+        else if (self.gameGuess == 3){
+            self.won3 = won3+1
+        }
+        else if (self.gameGuess == 4){
+            self.won4 = won4+1
+        }
+        else if (self.gameGuess == 5){
+            self.won5 = won5+1
+        }
+        else if (self.gameGuess == 6){
+            self.won6 = won6+1
+        }
+        else if (self.gameGuess == 7){
+            self.won7 = won7+1
+        }
+        else if (self.gameGuess == 8){
+            self.won8 = won8+1
+        }
+        
+        self.everyGuess = everyGuess + gameGuess
+        self.aver = (everyGuess / gamesP)
     }
 }
