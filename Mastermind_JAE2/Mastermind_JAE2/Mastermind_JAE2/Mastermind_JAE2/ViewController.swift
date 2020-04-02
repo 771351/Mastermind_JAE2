eb54443456fd8df3557d93e637a797cc5ef498f3
771351 <58735806+771351@users.noreply.github.com>
2020-04-01 22:23:32 -0500

    switched how popup for team name works

--- Mastermind_JAE2/Mastermind_JAE2/Mastermind_JAE2/Mastermind_JAE2/ViewController.swift
+++ Mastermind_JAE2/Mastermind_JAE2/Mastermind_JAE2/Mastermind_JAE2/ViewController.swift
@@ -10,19 +10,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var Totalview: UIView!
+    @IBOutlet weak var backgroundButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        ViewTNa.layer.cornerRadius = 40
     }
-//popup to add new team
+    //popup to add new team
+    @IBOutlet weak var ViewTNa: UIView!
+    
     @IBAction func showPopUp(_ sender: UIButton) {
-        
-        let popOverVC = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "sbPopUpID") as! PopUpViewController
-        self.addChild(popOverVC)
-        popOverVC.view.frame = self.view.frame
-        self.view.addSubview(popOverVC.view)
-        popOverVC.didMove(toParent: self)
+        ViewTNa.frame.origin.x = 192
+        backgroundButton.alpha = 0.7
     }
+    //moves popup off screen
+    @IBAction func closebuttom (_ sender: Any) {
+        ViewTNa.frame.origin.x = -500
+        backgroundButton.alpha = 0
+    }
+    //moves popup off screen
+    @IBAction func goteamname(_ sender: Any) {
+        ViewTNa.frame.origin.x = -500
+        backgroundButton.alpha = 0
+    }
+    
+    
+    
     //timer popup
     @IBAction func showSPopUp(_ sender: UIButton) {
         let popOverVC = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "sbSPopUpID") as! SecondPopUpViewController
